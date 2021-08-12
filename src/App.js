12131a024e19a66fb780338bb74c34ea71df2594
= <Vamsi.duvvuri@icloud.com>
2021-08-12 14:53:30 -0400

    v0 - started converting to personal notes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API, Storage } from 'aws-amplify';
+// import { API, Storage } from 'aws-amplify';
+import { API} from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listTodos } from './graphql/queries';
 import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
@@ -10,7 +11,6 @@ const initialFormState = { name: '', description: '' }
 function App() {
   const [todos, setTodos] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
-
   useEffect(() => {
     fetchTodos();
   }, []);
@@ -19,10 +19,10 @@ function App() {
   const apiData = await API.graphql({ query: listTodos });
   const todosFromAPI = apiData.data.listTodos.items;
   await Promise.all(todosFromAPI.map(async todo => {
-    if (todo.image) {
-      const image = await Storage.get(todo.image);
-      todo.image = image;
-    }
+    // if (todo.image) {
+    //   const image = await Storage.get(todo.image);
+    //   todo.image = image;
+    // }
     return todo;
   }))
   setTodos(apiData.data.listTodos.items);
@@ -31,10 +31,10 @@ function App() {
   async function createTodo() {
   if (!formData.name || !formData.description) return;
   await API.graphql({ query: createTodoMutation, variables: { input: formData } });
-  if (formData.image) {
-    const image = await Storage.get(formData.image);
-    formData.image = image;
-  }
+  // if (formData.image) {
+  //   const image = await Storage.get(formData.image);
+  //   formData.image = image;
+  // }
   setTodos([ ...todos, formData ]);
   setFormData(initialFormState);
 }
@@ -46,45 +46,40 @@ function App() {
   }
 
   async function onChange(e) {
-	if (!e.target.files[0]) return
-	const file = e.target.files[0];
-	setFormData({ ...formData, image: file.name });
-	await Storage.put(file.name, file);
+	// if (!e.target.files[0]) return
+	// const file = e.target.files[0];
+	// setFormData({ ...formData, image: file.name });
+	// await Storage.put(file.name, file);
 	fetchTodos();
   }
 
   return (
     <div className="App">
-      <h1>My Todos App</h1>
-      <input
-        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Todo name"
-        value={formData.name}
-      />
-      <input
-        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Todo description"
-        value={formData.description}
-      />
-      <input
-  		type="file"
-  		onChange={onChange}
-	  />
-      <button onClick={createTodo}>Create Todo</button>
+      <h1>My Notes App</h1>
+      <input onChange={e => setFormData({ ...formData, 'name': e.target.value})} placeholder="name" value={formData.name} />
+      <input onChange={e => setFormData({ ...formData, 'description': e.target.value})} placeholder="description" value={formData.description} />
+      <button onClick={createTodo}>Create note</button>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <button>shuffle</button>
+      <button>oldest first</button>
+      <button>least read first</button>
+      
+      <table border="1"> 
       <div style={{marginBottom: 30}}>
         {
-		  todos.map(todo => (
-		    <div key={todo.id || todo.name}>
-		      <h2>{todo.name}</h2>
-		      <p>{todo.description}</p>
-		      <button onClick={() => deleteTodo(todo)}>Delete todo</button>
-		      {
-		        todo.image && <img src={todo.image} style={{width: 400}} alt=""/>
-		      }
-		    </div>
-		  ))
-		}
+    		  todos.map(todo => (
+            <tr>
+              <td width="200"><b>{todo.name}</b></td>
+              <td width="400">{todo.description}</td>
+              <td width="50"><button onClick={() => deleteTodo(todo)}>delete</button></td>
+            </tr>
+    		  ))
+		    }
       </div>
+      </table>
       <AmplifySignOut />
     </div>
   );
